c32f8b139f5ca103ce196ca6017829aa82b1d486
Patrick Vilhena <plvilhena@gmail.com>
2016-09-23 08:46:12 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+##About##
+
+This is a plugin I wrote to monitor my Circle builds from my menu bar like a lazy person. It used to be better written, but it didn’t really work well with the number of builds that are run here, so I hacked together an update quickly. Hope you find it helpful.
+
+NOTE: This used to use the `recent-builds` Circle API endpoint, but that only queries the 30 most recent builds. Given the number of builds run at UT daily, this would pretty much ensure no one ever saw their builds in Bitbar. I rewrote the script to use the `projects` endpoint instead. This has the side effect that every time a build of master is being run, it'll show up for everyone who has contributed to that project. Not necessarily a bad thing, we can all keep an eye on master.
+
+##Installation##
+
+1. Download and install Bitbar: https://github.com/matryer/bitbar (you’ll be asked to create a folder for your bitbar plugins during install)
+
+2. Move the `circle.5s.py` file to the plugin folder (DON’T MOVE THIS README THERE)
+
+3. Edit `circle.5s.py` file and add in your Circle CI API token (you can get one here https://circleci.com/account/api)
+
+4. Add your GitHub username (the one you use to login) to your gitconfig under [user], e.g.
+    ```
+      [user]
+        username = nifengle
+    ```
+
+5. `chmod +x circle.5s.py`
+
+6. Click on Bitbar in the menu bar, then Preferences > Refresh all. Watch your build statuses from your menu bar. Click the latest build name on the menu bar to see a dropdown of your 10 most recent builds (across all projects you’re subscribed to in Circle). Clicking on a build will open your browser to that build’s url.
---
+++ circle.5s.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# <bitbar.title>CircleCI Build Status</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Patrick Vilhena</bitbar.author>
+# <bitbar.author.github>Nifengle</bitbar.author.github>
+# <bitbar.desc>Shows the status of your CircleCI builds.</bitbar.desc>
+# <bitbar.dependencies>python</bitbar.dependencies>
+
+import os
+import urllib2
+import json
+from operator import itemgetter
+
+# Go to circleci.com/account/api and create an API token
+# Paste that value in here. BitBar can't seem to access environment
+# variables. See https://github.com/matryer/bitbar/issues/88
+circle_token = ''
+
+base_url = 'https://circleci.com/api/v1'
+git_username = os.popen('git config user.username').readline().strip()
+
+status_colors = {
+    'failed': 'red',
+    'success': 'green',
+    'fixed': 'green',
+    'running': '#66D3E4',
+    'scheduled': 'purple',
+    'not_running': 'purple',
+    'timedout': 'yellow',
+    'canceled': 'orange'
+}
+
+status_symbols = {
+    'failed': u'\u2718 ',
+    'success': u'\u2713',
+    'fixed': u'\u2713',
+    'running': u'\u25b6',
+    'timedout': u'\u26A0',
+    'scheduled': u'\u275a\u275a',
+    'not_running': u'\u275a\u275a',
+    'canceled': u'\u20e0'
+}
+
+def fetch_projects():
+    url = '%s/projects?circle-token=%s' % (base_url, circle_token)
+    request = urllib2.Request(url, headers = { 'Accept': 'application/json' })
+    response = urllib2.urlopen(request).read()
+    projects = json.loads(response)
+    return projects
+
+def user_branches():
+    projects = fetch_projects()
+    branches = {}
+    for project in projects:
+        for branch_name, branch_info in project['branches'].iteritems():
+            if is_own_branch(branch_info):
+                branch_info['reponame'] = project['reponame']
+                branch_name = urllib2.unquote(branch_name).encode('utf-8')
+                branches[branch_name] = branch_info
+
+    return branches
+
+def is_own_branch(branch):
+    committer_usernames = branch['pusher_logins']
+    return git_username in committer_usernames
+
+def user_builds(branches):
+    builds = []
+
+    # Show all builds, not just the most recent one
+    for name, info in branches.iteritems():
+        if len(info['running_builds']) > 0:
+            running_build = info['running_builds'][0]
+            running_build['branch'] = name
+            running_build['reponame'] = info['reponame']
+            builds.append(running_build)
+
+        if 'recent_builds' in info:
+            for build in sorted(info['recent_builds'], key = itemgetter('added_at'), reverse = True):
+                build['branch'] = name
+                build['reponame'] = info['reponame']
+                builds.append(build)
+    return builds
+
+
+def format_output(build, is_link = True):
+    status = build['status']
+    color = status_colors.get(status, 'yellow')
+    symbol = status_symbols.get(status, u'\ufe0f')
+    branch_name = build['branch'][:20]
+    url = 'https://circleci.com/gh/usertesting/%s/%s' % (build['reponame'], build['build_num'])
+    build_num = build['build_num']
+    outcome = '%(symbol)s  %(branch_name)s - %(build_num)s | color=%(color)s' % locals()
+    if is_link:
+        outcome = '%(outcome)s href=%(url)s' % locals()
+    return outcome.encode('utf-8')
+
+if __name__ == '__main__':
+
+    if len(circle_token) == 0:
+        raise ValueError('Token can not be empty')
+
+    user_branches = user_branches()
+    user_builds = user_builds(user_branches)
+    latest_builds = sorted(user_builds, key = itemgetter('added_at'), reverse = True)
+    builds = latest_builds[:10]
+    if len(builds) == 0:
+        print 'No Builds'
+    else:
+        print format_output(builds[0], False)
+
+        print '---'
+
+        for build in builds:
+            print format_output(build)
